46bea75728d238ade0ee0ac92a66ee766ad36be3
Office PC <72garmartin@gmail.com>
2022-01-03 17:20:03 +0700

    #2 Login screen design

--- app/src/main/java/com/siregarmartin/loginwithmvvm/MainActivity.kt
+++ app/src/main/java/com/siregarmartin/loginwithmvvm/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.siregarmartin.loginwithmvvm
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.siregarmartin.loginwithmvvm.ui.auth.AuthActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        finish()
+        startActivity(Intent(this, AuthActivity::class.java))
     }
 }
