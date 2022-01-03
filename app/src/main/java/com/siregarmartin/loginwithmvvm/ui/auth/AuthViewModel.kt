f9e9c3e510988a085060f6eb5ac3321852fe1e95
Office PC <72garmartin@gmail.com>
2022-01-03 18:18:39 +0700

    View Model

---
+++ app/src/main/java/com/siregarmartin/loginwithmvvm/ui/auth/AuthViewModel.kt
@@ -0,0 +1,11 @@
+package com.siregarmartin.loginwithmvvm.ui.auth
+
+import androidx.lifecycle.ViewModel
+import com.siregarmartin.loginwithmvvm.repository.AuthRepository
+
+class AuthViewModel(
+    private val repository: AuthRepository
+) : ViewModel() {
+
+
+}
---
+++ app/src/main/java/com/siregarmartin/loginwithmvvm/ui/base/ViewModelFactory.kt
@@ -0,0 +1,21 @@
+package com.siregarmartin.loginwithmvvm.ui.base
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.siregarmartin.loginwithmvvm.repository.AuthRepository
+import com.siregarmartin.loginwithmvvm.repository.BaseRepository
+import com.siregarmartin.loginwithmvvm.ui.auth.AuthViewModel
+import java.lang.IllegalArgumentException
+
+class ViewModelFactory(
+    private val repository: BaseRepository
+) : ViewModelProvider.NewInstanceFactory() {
+
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        return when {
+            modelClass.isAssignableFrom(AuthViewModel::class.java) ->
+                AuthViewModel(repository as AuthRepository) as T
+            else -> throw  IllegalArgumentException("ViewModelClass Not Found")
+        }
+    }
+}
