67da10796c2cb4bc934d1d70d26bd4e76159d813
Office PC <72garmartin@gmail.com>
2022-01-06 10:49:52 +0700

    switching the Coroutine ti Dispatchers.IO

--- app/src/main/java/com/siregarmartin/loginwithmvvm/ui/base/BaseViewModel.kt
+++ app/src/main/java/com/siregarmartin/loginwithmvvm/ui/base/BaseViewModel.kt
@@ -3,10 +3,12 @@ package com.siregarmartin.loginwithmvvm.ui.base
 import androidx.lifecycle.ViewModel
 import com.siregarmartin.loginwithmvvm.data.network.UserApi
 import com.siregarmartin.loginwithmvvm.data.repository.BaseRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 
 abstract class BaseViewModel(
     private val repository: BaseRepository
 ) : ViewModel() {
 
-    suspend fun logout(api: UserApi) = repository.logout(api)
+    suspend fun logout(api: UserApi) = withContext(Dispatchers.IO) {repository.logout(api)}
 }
